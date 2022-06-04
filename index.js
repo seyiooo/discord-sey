704eedceabad0cff076f39b8331702531ee42127
seyioo <81486074+seyiooo@users.noreply.github.com>
2022-06-04 10:54:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('./src/Prototype');
+
 module.exports = {
     MemberCount: require('./src/structures/MemberCount/index'),
     Pagination: require('./src/structures/Pagination/index'),
+    RankCard: require('./src/structures/RankCard/index'),
     Starboard: require('./src/structures/Starboard/index'),
     TempVoc: require('./src/structures/TempVoc/index')
 };
